88f91c9665265dc041863c5f80d93b1b65dcb9d6
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-10-20 17:05:56 -0400

    added middleware

---
+++ config/middleware.js
@@ -0,0 +1,15 @@
+module.exports = {
+  load: {
+    before: ['responseTime', 'boom', 'logger', 'cors', 'responses', 'gzip'],
+    after: ['parser', 'router'],
+  },
+  settings: {
+    gzip: {
+      enabled: true,
+      options: {
+        br: false
+      }
+    }
+  }
+};
+
